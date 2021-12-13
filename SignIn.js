a1b439dac1960556791996148086eecde8d38b14
Sabotron <77740697+Sabotron@users.noreply.github.com>
2021-12-13 11:46:42 -0600

    Add files via upload
    - Se eliminpó bug que impedía acceder desde el LogIn
    - Gestion de productos, almacena en memoria el id de cada producto junto al de su creador

--- SignIn.js
+++ SignIn.js
@@ -1,6 +1,6 @@
 let counter = 0;
 let session_id = 0;
-
+let product_id = 0;
 
 function consecutive() {
     let new_counter = JSON.parse(localStorage.getItem("consecutive"));
@@ -11,9 +11,21 @@ function consecutive() {
     }
 }
 /*-------------------------------------------------------------------------*/
-
+function actual_session() {
+    let session = JSON.parse(localStorage.getItem("session_id"));
+        session_id = session;
+}
+/*-------------------------------------------------------------------------*/
+function last_product() {
+    let product = JSON.parse(localStorage.getItem("lastest_product"));
+    if (!product) {
+        product = 0;
+    } else {
+        product_id = product;
+    }
+}
+/*-------------------------------------------------------------------------*/
 function check_info() {
-
     if (document.getElementById("nombre").value.length >= 3 &&
         document.getElementById("apellido").value.length >= 3 &&
         document.getElementById("address1").value.length >= 3 &&
@@ -27,9 +39,20 @@ function check_info() {
 }
 
 /*-------------------------------------------------------------------------*/
-
+function check_description() {
+    if (document.getElementById("producto").value.length >= 3 &&
+        document.getElementById("categoria").value.length >= 3 &&
+        document.getElementById("descripcion").value.length >= 3 &&
+        document.getElementById("descripcion").value.length <= 300 &&
+        document.getElementById("interes").value.length >= 3 &&
+        document.getElementById("interes").value.length <= 300) {
+        document.getElementById("btn_guardar").disabled = false;
+    } else {
+        document.getElementById("btn_guardar").disabled = true;
+    }
+}
+/*-------------------------------------------------------------------------*/
 function get_user() {
-
     counter = counter + 1;
     let registry_db = JSON.parse(localStorage.getItem("registry"));
     if (!registry_db) {
@@ -47,24 +70,18 @@ function get_user() {
         email: document.getElementById("email").value,
         passw: document.getElementById("password").value
     }
-
     registry_db.push(new_user);
     localStorage.setItem("registry", JSON.stringify(registry_db));
     localStorage.setItem("consecutive", counter);
     alert("Nuevo usuario creado: " +
         new_user.name + " " + new_user.last_name);
-    window.location.href = "Login.html";
     clear_input();
-
+    window.location.href = "Login.html"; 
 }
-
 /*-------------------------------------------------------------------------*/
-
 function check_user() {
     let registry_db = JSON.parse(localStorage.getItem("registry"));
-    if (!registry_db) {
-        auxiliar_catch();
-    } else {
+    if (registry_db) {
         var user_email = document.getElementById("email").value;
         var user_passwd = document.getElementById("password").value;
         for (i = 0; i < registry_db.length; i++) {
@@ -73,39 +90,17 @@ function check_user() {
                 let user_id = registry_db[i].id;
                 localStorage.setItem("session_id", user_id);
                 window.location.href = "Dashboard.html";
-                break;
-            }
-            else {
-                auxiliar_catch();
+                clear_login();
                 break;
             }
         }
+        clear_login();
+    } else {
+        auxiliar_catch();
     }
 }
-
 /*-------------------------------------------------------------------------*/
-
-function auxiliar_catch() {
-    alert("El usuario no se encuentra registrado!");
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
-
-}
-/*-------------------------------------------------------------------------*/
-
-function clear_login() {
-
-    document.getElementById("email").value = "";
-    document.getElementById("password").value = "";
-
-}
-
-/*-------------------------------------------------------------------------*/
-
-
-
 function clear_input() {
-
     document.getElementById("btn_registrar").disabled = true;
     document.getElementById("nombre").value = "";
     document.getElementById("apellido").value = "";
@@ -116,8 +111,42 @@ function clear_input() {
     document.getElementById("ciudad").value = "";
     document.getElementById("email").value = "";
     document.getElementById("password").value = "";
-
 }
-
+/*-------------------------------------------------------------------------*/
+function get_product() {
+    last_product();
+    product_id = product_id + 1;
+    let inventory_db = JSON.parse(localStorage.getItem("inventory"));
+    if (!inventory_db) {
+        inventory_db = [];
+    }
+    actual_session();
+    const new_product = {
+        id: product_id,
+        user_id: session_id,
+        product: document.getElementById("producto").value,
+        category: document.getElementById("categoria").value,
+        description: document.getElementById("descripcion").value,
+        interest: document.getElementById("interes").value
+    }
+    inventory_db.push(new_product);
+    localStorage.setItem("inventory", JSON.stringify(inventory_db));
+    localStorage.setItem("lastest_product", product_id);
+    alert("Nuevo producto creado: " +
+    new_product.product + " de tipo " + new_product.category);
+    window.location.href = "Dashboard.html";
+    //clear_input();
+}
+/*-------------------------------------------------------------------------*/
+function auxiliar_catch() {
+    alert("Ningun usuario se encuentra registrado!");
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+}
+/*-------------------------------------------------------------------------*/
+function clear_login() {
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+}
 /*-------------------------------------------------------------------------*/
 consecutive();
